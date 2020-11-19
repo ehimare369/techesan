75a8d42c30db9f5e366ea142258df3bac10ebd2b
Ehimare Okosun <ehimare369@gmail.com>
2020-11-19 18:24:18 +0100

    First commit. Finished with the desktop view navigation bar

---
+++ app.js
@@ -0,0 +1,15 @@
+// Selection of     HTML objects
+const mobileMenu = document.querySelector('.navbar-toggle i');
+const navbarMenu = document.querySelector('.navbar-menu');
+
+// Defining function
+const toggleNavbar = () => {
+    mobileMenu.classList.toggle('fa-bars');
+    mobileMenu.classList.toggle('fa-times');
+    navbarMenu.classList.toggle('navbar-active');
+}
+
+// Calling the fucntion after click event occurs
+mobileMenu.addEventListener('click', () => {
+    toggleNavbar();
+});
