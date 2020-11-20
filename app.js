6129b1d0544ced5618df7fb1f1fe8c900c85a9e4
Ehimare Okosun <ehimare369@gmail.com>
2020-11-20 21:29:48 +0100

    Finished with the about section, included the media query section too

--- app.js
+++ app.js
@@ -4,12 +4,10 @@ const navbarMenu = document.querySelector('.navbar-menu');
 
 // Defining function
 const toggleNavbar = () => {
-    mobileMenu.classList.toggle('fa-bars');
+    // mobileMenu.classList.toggle('fa-bars');
     mobileMenu.classList.toggle('fa-times');
     navbarMenu.classList.toggle('navbar-active');
 }
 
 // Calling the fucntion after click event occurs
-mobileMenu.addEventListener('click', () => {
-    toggleNavbar();
-});
+mobileMenu.addEventListener('click', toggleNavbar);
